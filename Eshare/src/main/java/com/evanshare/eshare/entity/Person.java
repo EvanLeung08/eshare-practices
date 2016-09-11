2b8372ff1af625d97fcd868a942c98e96e3a66ea
10856214 <10856214@163.com>
2016-09-12 01:07:22 +0800

    Evan:新增:框架设计教程例子

---
+++ Eshare/src/main/java/com/evanshare/eshare/entity/Person.java
@@ -0,0 +1,31 @@
+package com.evanshare.eshare.entity;
+
+/**
+ * Created by liangyh on 2016/9/11.
+ * Email:10856214@163.com
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
