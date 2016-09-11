e1c9a0e10a9d151edd1307fc523a3e1af69d8797
10856214 <10856214@163.com>
2016-09-12 01:08:02 +0800

    Evan:删除：去除冗余包导入

--- Eshare/src/test/java/com/evanshare/eshare/entity/reflect/ReflectApiTest.java
+++ Eshare/src/test/java/com/evanshare/eshare/entity/reflect/ReflectApiTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
