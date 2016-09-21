d967a87900da3425b7feed497fccfaec6ce0fdc5
10856214 <10856214@163.com>
2016-09-21 08:06:38 +0800

    Evan: 新增:EshareFramework框架工程

---
+++ EshareFramework/src/main/java/com/eshare/core/filter/dispatchFilter.java
@@ -0,0 +1,24 @@
+package com.eshare.core.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * 核心请求分发过滤器
+ * Created by liangyh on 2016/9/21.
+ * Email:10856214@163.com
+ */
+public class DispatchFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+}
